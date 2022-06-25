43b17d1441d5e75a481eca595b3b5eb90f2cb031
gabrielruz43 <gabrielruz43@gmail.com>
2022-06-25 20:24:32 +0300

    Added ViewImports example.

---
+++ ViewImports/Models/Person.cs
@@ -0,0 +1,4 @@
+﻿namespace ViewImports.Models
+{
+    public record class Person(string Name, int Age);
+}
---
+++ ViewImports/Program.cs
@@ -0,0 +1,9 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+var app = builder.Build();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
